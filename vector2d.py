639c80e176dc8817998e2fdeca6f6d34f1fc8b5c
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2018-02-01 23:26:35 +0700

    Methods

---
+++ vector2d.py
@@ -0,0 +1,71 @@
+import math
+
+class Vector2D:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+    
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def __imul__(self, other):
+        self.x *= other.x
+        self.y *= other.y
+        return self
+
+    def __div__(self, other):
+        return Vector2D(self.x/float(other.x), self.y/float(other.y))
+
+    def getLength(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def normalized(self):
+        length = self.getLength()
+        if length != 0:
+            return self/Vector2D(length,length)
+        return Vector2D(self)
+
+    def getAngle(self):
+        return math.degrees(math.atan2(self.y, self.y))
+
+    def __str__(self):
+        return "x:"+ str(self.x) +",y:"+ str(self.y)
+
+def Main():
+    vec = Vector2D(5,6)
+    vec2 = Vector2D(2,3)
+    print vec
+    print vec2
+
+    tempmethod = vec.getAngle
+
+    vec = vec + vec2
+    print vec
+
+    vec += vec2
+    print vec
+
+    vec *= Vector2D(2,2)
+    print vec
+
+    print vec.normalized()
+    print vec.getAngle()
+
+    print tempmethod()
+
+if __name__ == "__main__":
+    Main()
